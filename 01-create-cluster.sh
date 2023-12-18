d67e54c4e987601b9af69a5764dc77bf00eee3e0
hcltech84 <97866525+hcltech84@users.noreply.github.com>
2023-12-18 10:04:43 +0700

    create-cluster #1 (#2)

---
+++ 01-create-cluster.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install Google Cloud CLI or upgrade if already installed
+if ! command -v -- gcloud > /dev/null 2>&1; then
+  brew install --cask google-cloud-sdk
+else
+  gcloud components updates
+fi
+
+# Initialize Google Cloud CLI
+gcloud init
+
+# Declare envars
+export PROJECT_NAME="voting-app"
+export CLUSTER_NAME=$PROJECT_NAME
+export PROJECT_ID="voting-app-408302"
+export REGION="asia-southeast1"
+export ZONE="asia-southeast1-a"
+export BILLING_ACCOUNT_ID="019953-399CC3-B5B87A"
+
+# Create a new Google Cloud project
+gcloud projects create $PROJECT_ID --name=$PROJECT_NAME
+
+# Enable Billing for new project
+gcloud alpha billing projects link $PROJECT_ID --billing-account=$BILLING_ACCOUNT_ID
+
+# Enable Kubernetes API for new project
+SERVICE_NAME=container.googleapis.com
+gcloud services enable $SERVICE_NAME --project=$PROJECT_ID
+
+# Create a standard, single-zone cluster with `HttpLoadBalancing` add-on disabled.
+gcloud container clusters create $CLUSTER_NAME --addons="HorizontalPodAutoscaling" --zone $ZONE --project $PROJECT_ID
+# --addons="HorizontalPodAutoscaling"
+#   As we'll use Ingress Nginx rather than GKE Ingress, `HttpLoadBalancing` add-on should be disabled to prevents 
+#   GKE Ingress to process Ingress resources.
+#   The default addons are `"HttpLoadBalancing, HorizontalPodAutoscaling"`, 
+#   explicitly specifies `HorizontalPodAutoscaling` will disable `HttpLoadBalancing`
+#   For more detail about `--addons` flag, see https://cloud.google.com/sdk/gcloud/reference/container/clusters/create
+
+# Clean up
+gcloud projects delete $PROJECT_ID
